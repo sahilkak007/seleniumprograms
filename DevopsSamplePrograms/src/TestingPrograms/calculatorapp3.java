b910488c44387d806e91f20cb77bfef160d15e38
sahil.kak <sahilkak007@gmail.com>
2019-06-23 16:30:26 +0530

    this is new file

---
+++ DevopsSamplePrograms/src/TestingPrograms/calculatorapp3.java
@@ -0,0 +1,15 @@
+package TestingPrograms;
+
+import org.testng.annotations.Test;
+
+public class calculatorapp3 {
+
+	@Test
+	public void  div()
+	{
+		
+		
+		System.out.println("Div test case");
+	}
+	
+}
