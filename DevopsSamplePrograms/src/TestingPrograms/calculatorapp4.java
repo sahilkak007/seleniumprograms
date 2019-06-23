c165324aef5f700fd0e95e947ec4ffea8c31ab6e
sahil.kak <sahilkak007@gmail.com>
2019-06-23 16:51:01 +0530

    JIRA-3456-new app module

---
+++ DevopsSamplePrograms/src/TestingPrograms/calculatorapp4.java
@@ -0,0 +1,15 @@
+package TestingPrograms;
+
+import org.testng.annotations.Test;
+
+public class calculatorapp4 {
+
+	
+	@Test
+	public void login()
+	{
+		
+		
+		System.out.println("login test case is successful");
+	}
+}
